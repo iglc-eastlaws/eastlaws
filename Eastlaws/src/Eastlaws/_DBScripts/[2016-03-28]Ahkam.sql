fcd1e7fe0536a71c837d1f976eb12d4f3c8ae248
Besada Hanna <dotnet88@hotmail.com>
2016-03-28 14:54:16 +0200

    test edit scripts

--- Eastlaws/src/Eastlaws/_DBScripts/[2016-03-28]Ahkam.sql
+++ Eastlaws/src/Eastlaws/_DBScripts/[2016-03-28]Ahkam.sql
@@ -2,9 +2,22 @@
 
 Go 
 
+Create Table Services
+(ID int Primary Key , ServiceName nvarchar(256))
+
+Go
+
+
 
 Create Table ServicesCountries 
 (ID int Primary Key , Name nvarchar(512), EnName nvarchar(512) , FlagPic varchar(64) )
+
+Go 
+
+
+
+
+Use EastlawsUsers 
 
 Go 
 
