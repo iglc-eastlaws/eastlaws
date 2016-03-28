b3577b5197143a64e9693dd6ae3ae7af400645e0
alnahas <alnahas1@hotmail.com>
2016-03-28 17:47:23 +0200

    db_script

--- Eastlaws/src/Eastlaws/Services/RestaurantData.cs
+++ Eastlaws/src/Eastlaws/Services/RestaurantData.cs
@@ -28,12 +28,14 @@ namespace Eastlaws.Services
 
         public void Add(Restaurant newRestaurant)
         {
+            
             newRestaurant.Id = _restaurants.Max(r => r.Id) + 1;
            _restaurants.Add(newRestaurant);
         }
 
         public Restaurant Get(int id)
         {
+ 
             return _restaurants.FirstOrDefault(r => r.Id == id);
         }
 
