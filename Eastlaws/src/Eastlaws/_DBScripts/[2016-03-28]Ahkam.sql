0154aad046652ff8f52d3c0b3651d6d53a4a5dd4
Besada Hanna <dotnet88@hotmail.com>
2016-03-28 14:48:59 +0200

    Initial Database script Creation

---
+++ Eastlaws/src/Eastlaws/_DBScripts/[2016-03-28]Ahkam.sql
@@ -0,0 +1,12 @@
+﻿Use EastlawsData 
+
+Go 
+
+
+Create Table ServicesCountries 
+(ID int Primary Key , Name nvarchar(512), EnName nvarchar(512) , FlagPic varchar(64) )
+
+Go 
+
+
+
---
+++ Eastlaws/src/Eastlaws/_DBScripts/[2016-03-28]CreateDBs.sql
@@ -0,0 +1,14 @@
+﻿Use master
+
+Go 
+
+Create DataBase EastlawsData 
+
+Go 
+
+
+Create DataBase EastlawsUsers 
+
+Go 
+
+
