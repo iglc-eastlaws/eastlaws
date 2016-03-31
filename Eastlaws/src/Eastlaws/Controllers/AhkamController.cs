f8af000c47478bf5583bbad907815d16247a5dc3
Besada Hanna <dotnet88@hotmail.com>
2016-03-31 15:14:15 +0200

    testing

--- Eastlaws/src/Eastlaws/Controllers/AhkamController.cs
+++ Eastlaws/src/Eastlaws/Controllers/AhkamController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
 using Eastlaws.ViewModels.Ahkam;
+using System.Data;
+using System.Data.SqlClient;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -31,6 +33,29 @@ namespace Eastlaws.Controllers
             return View(Model);
         }
 
+
+        public IActionResult Get(int? PageNo )
+        {
+            if (!PageNo.HasValue)
+                PageNo = 1;
+
+            string Query = "SELECT * FROM dbo.AH_Ma7akem am ";
+            DataTable Table =  GetTable(Query);
+            return View(Table);
+
+            
+
+        }
+        private DataTable GetTable(string Query)
+        {
+            SqlConnection connection = new SqlConnection("Initial Catalog=iglc;Data Source=192.168.1.250;User ID=testDev;Password=test@myDev123;Asynchronous Processing=true;");
+            SqlDataAdapter adapter = new SqlDataAdapter(Query, connection);
+            DataTable dtData = new DataTable();
+            adapter.Fill(dtData);
+            return dtData;
+
+
+        }
 
     }
 }
