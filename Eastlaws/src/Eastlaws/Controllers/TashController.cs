ac0ea751f9b0fd789539649d7a1dd52eeeefb247
alnahas <alnahas1@hotmail.com>
2016-03-31 11:37:49 +0200

    Tash with dapper.net test

--- Eastlaws/src/Eastlaws/Controllers/TashController.cs
+++ Eastlaws/src/Eastlaws/Controllers/TashController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
+using Eastlaws.Entities;
+
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,10 +12,18 @@ namespace Eastlaws.Controllers
 {
     public class TashController : Controller
     {
+        Tash t = new Tash();
         // GET: /<controller>/
         public IActionResult Index()
         {
-            return View();
+            var model = t.GetAll();
+            return View(model);
+        }
+
+        public IActionResult Details(int ID)
+        {
+            var model = t.Find(ID);
+            return View(model);
         }
     }
 }
