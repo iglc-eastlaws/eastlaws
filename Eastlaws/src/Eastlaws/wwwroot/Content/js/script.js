77e8d0d27abd8808f8d2ca5fce639a3bfc657793
nour <esbtan@cazamiza.com>
2016-04-03 17:25:44 +0200

    Build HTML&CSS Home page

---
+++ Eastlaws/src/Eastlaws/wwwroot/Content/js/script.js
@@ -0,0 +1,39 @@
+﻿// JavaScript Document
+(function ($) {
+    $(function () {
+
+        var selectFormGroup = function (event) {
+            event.preventDefault();
+
+            var $selectGroup = $(this).closest('.input-group-select');
+            var param = $(this).attr("href").replace("#", "");
+            var concept = $(this).text();
+
+            $selectGroup.find('.concept').text(concept);
+            $selectGroup.find('.input-group-select-val').val(param);
+
+        }
+
+        $(document).on('click', '.dropdown-menu a', selectFormGroup);
+
+    });
+})(jQuery);
+$('#myCarousel').carousel({
+    interval: 10000
+})
+
+$('.carousel .item').each(function () {
+    var next = $(this).next();
+    if (!next.length) {
+        next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+
+    if (next.next().length > 0) {
+        next.next().children(':first-child').clone().appendTo($(this));
+    }
+    else {
+        $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+    }
+});
+
