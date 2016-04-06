f1bd8bb3d280646d68e8022427f559c6718f823a
nour <esbtan@cazamiza.com>
2016-04-06 17:05:16 +0200

    add functons Filter

--- Eastlaws/src/Eastlaws/wwwroot/Content/js/script.js
+++ Eastlaws/src/Eastlaws/wwwroot/Content/js/script.js
@@ -1,5 +1,8 @@
 ﻿// JavaScript Document
 (function ($) {
+    // Tooltip
+    $('[data-toggle="tooltip"]').tooltip();
+
     $(function () {
 
         var selectFormGroup = function (event) {
@@ -22,31 +25,41 @@ $('#myCarousel').carousel({
     interval: 10000
 })
 $(document).ready(function () {
-$('.carousel .item').each(function () {
-    var next = $(this).next();
-    if (!next.length) {
-        next = $(this).siblings(':first');
-    }
-    next.children(':first-child').clone().appendTo($(this));
+    $('.carousel .item').each(function () {
+        var next = $(this).next();
+        if (!next.length) {
+            next = $(this).siblings(':first');
+        }
+        next.children(':first-child').clone().appendTo($(this));
 
-    if (next.next().length > 0) {
-        next.next().children(':first-child').clone().appendTo($(this));
-    }
-    else {
-        $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
-    }
-});
+        if (next.next().length > 0) {
+            next.next().children(':first-child').clone().appendTo($(this));
+        }
+        else {
+            $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+        }
+    });
 
-$("#slide-slides").cslide();
-$("#cslide-slides").cslide();
-// Scroll 
-$(".filter-search .well").mCustomScrollbar({
-    setHeight: "200px",
-    theme: "3d",
-    scrollButtons: "1"
+    $("#slide-slides").cslide();
+    $("#cslide-slides").cslide();
+    // Scroll 
+    $(".filter-search .well").mCustomScrollbar({
+        setHeight: "200px",
+        theme: "3d",
+        scrollButtons: "1"
 
-});
+    });
     // muilt check
+
+    // Hide Element Filter
+    $(".filter-element ul li a i").click(function () {
+        $(this).parent().parent().fadeOut("slow", function () {
+            $(this).remove();
+        });
+    });
+
+
+
 
 
 });
