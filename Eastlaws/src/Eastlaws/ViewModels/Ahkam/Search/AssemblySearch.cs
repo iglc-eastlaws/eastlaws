ccb171407e28134ed9b672b6b2234c3f78167fb1
Nada AbuGamel <nobi_nh@hotmail.com>
2016-04-21 06:02:56 -0700

    model

---
+++ Eastlaws/src/Eastlaws/ViewModels/Ahkam/Search/AssemblySearch.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Eastlaws.ViewModels.Ahkam.Search
+{
+    public class AssemblySearch
+    {
+        public int? AnyTextType { get; set; }
+        public string AnyText { get; set; }
+    }
+}
