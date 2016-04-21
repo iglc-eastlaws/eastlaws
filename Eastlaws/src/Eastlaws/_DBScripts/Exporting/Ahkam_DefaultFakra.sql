6769057dfb8543958687d609b1601b0d5f36241b
Besada Hanna <dotnet88@hotmail.com>
2016-04-21 17:09:53 +0200

    Default Fakra Adjust + Export

---
+++ Eastlaws/src/Eastlaws/_DBScripts/Exporting/Ahkam_DefaultFakra.sql
@@ -0,0 +1,73 @@
+﻿Use EastlawsData
+
+Go 
+
+
+
+Update AhkamFakarat 
+Set IsDefault = null
+
+
+Update AhkamFakarat 
+Set IsDefault = 1 Where FakraNo = 1 
+
+
+Update AhkamFakarat 
+Set IsDefault = 1 
+Where HokmID in (
+	Select HokmID  From AhkamFakarat
+	Group By HokmID
+	Having Count(IsDefault) = 0 
+)
+And FakraNo = -1 
+
+
+
+Update AhkamFakarat 
+Set IsDefault = 1 
+Where HokmID in (
+	Select HokmID  From AhkamFakarat
+	Group By HokmID
+	Having Count(IsDefault) = 0 
+)
+And FakraNo = 0
+
+
+
+
+
+Update AhkamFakarat 
+Set IsDefault = 1 
+Where HokmID in (
+	Select HokmID  From AhkamFakarat
+	Group By HokmID
+	Having Count(IsDefault) = 0 
+)
+And FakraNo = -3 
+
+
+Update AhkamFakarat 
+Set IsDefault = 1 
+Where HokmID in (
+	Select HokmID  From AhkamFakarat
+	Group By HokmID
+	Having Count(IsDefault) = 0 
+)
+And FakraNo = -2 
+
+
+Update AhkamFakarat 
+Set IsDefault = 1 
+Where HokmID in (
+	Select HokmID  From AhkamFakarat
+	Group By HokmID
+	Having Count(IsDefault) = 0 
+)
+And FakraNo = -50 
+
+
+Update A 
+Set A.DefaultFakraID  = F.ID
+From   Ahkam  A 
+Join AhkamFakarat F  on F.HokmID  = A.ID and F.IsDefault = 1 
+
