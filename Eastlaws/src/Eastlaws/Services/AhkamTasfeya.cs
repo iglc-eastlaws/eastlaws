a39a39d3fe904e70e16e0ecd84e5959d9b94fdee
Besada Hanna <dotnet88@hotmail.com>
2016-04-23 19:57:57 +0200

    test

--- Eastlaws/src/Eastlaws/Services/AhkamTasfeya.cs
+++ Eastlaws/src/Eastlaws/Services/AhkamTasfeya.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace Eastlaws.Services
 {
-    public enum AhkamTasfeyaTypes
+    public enum AhkamTasfeyaCategories
     {
         Country = 0, Ma7kama = 1, CrimeType = 2, RelatedTash = 3, Fehres = 4, Defoo3 = 5, Year = 6, JudgeYear = 7, Ma7kamaAction = 8
     }
@@ -14,49 +14,83 @@ namespace Eastlaws.Services
         public int ID { get; set; }
         public string Name { get; set; }
         public int Count { get; set; }
-        public AhkamTasfeyaTypes Category { get; set; }
+        public AhkamTasfeyaCategories Category { get; set; }
     }
 
     public class AhkamTasfeya
     {
-        public static string GetSectionTitle(AhkamTasfeyaTypes theType)
+        public const int MAX_RECORD_COUNT_PER_CATEGORY = 100;
+
+
+        public static List<TasfeyaItem> List (int QueryID , AhkamSearchTypes SearchType , string TasfeyaFilter )
+        {
+            List<AhkamTasfeyaCategories> Cats = new List<AhkamTasfeyaCategories>();
+            Cats.Add(AhkamTasfeyaCategories.Country);
+            Cats.Add(AhkamTasfeyaCategories.Ma7kama);
+            Cats.Add(AhkamTasfeyaCategories.JudgeYear);
+            Cats.Add(AhkamTasfeyaCategories.Year);
+
+
+            string MasterQuery = "";
+
+            return null;
+        }
+
+        private static string GetCategoryQuery(int MasterQueryID , AhkamTasfeyaCategories Cat , string Filter, bool isSafyList = false)
+        {
+            switch (Cat )
+            {
+                case AhkamTasfeyaCategories.Country:
+                    {
+                        return null;
+  
+                    }
+                default:
+                    {
+                        return null;
+               
+                    }
+            }
+        }
+
+        public static string GetSectionTitle(AhkamTasfeyaCategories theType)
         {
             string Title = "";
             switch (theType)
             {
-                case AhkamTasfeyaTypes.Country:
+                case AhkamTasfeyaCategories.Country:
                     {
                         Title = "بالدولة"; break;
                     }
-                case AhkamTasfeyaTypes.Ma7kama:
+                case AhkamTasfeyaCategories.Ma7kama:
                     {
                         Title = "بالمحكمة"; break;
                     }
-                case AhkamTasfeyaTypes.CrimeType:
+                case AhkamTasfeyaCategories.CrimeType:
                     {
                         Title = "بنوع الدعوى/ الجريمة"; break;
                     }
-                case AhkamTasfeyaTypes.RelatedTash:
+                case AhkamTasfeyaCategories.RelatedTash:
                     {
                         Title = "بالقانون"; break;
                     }
-                case AhkamTasfeyaTypes.Fehres:
+                case AhkamTasfeyaCategories.Fehres:
                     {
                         Title = "بتصنيف المبادئ"; break;
                     }
-                case AhkamTasfeyaTypes.Defoo3:
+                case AhkamTasfeyaCategories.Defoo3:
                     {
                         Title = "بالدفوع"; break;
                     }
-                case AhkamTasfeyaTypes.Year:
+                case AhkamTasfeyaCategories.Year:
                     {
                         Title = "بسنة الحكم"; break;
                     }
-                case AhkamTasfeyaTypes.JudgeYear:
+                case AhkamTasfeyaCategories.JudgeYear:
                     {
                         Title = "بالسنة القضائية"; break;
                     }
-                case AhkamTasfeyaTypes.Ma7kamaAction:
+                case AhkamTasfeyaCategories.Ma7kamaAction:
                     {
                         Title = "بحكم المحكمة فى الدعوى"; break;
                     }
