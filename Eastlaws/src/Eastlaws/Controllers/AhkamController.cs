07222407819989a25606e6bca545678515925cb7
Besada Hanna <dotnet88@hotmail.com>
2016-05-29 16:36:02 +0200

    Fix View Mada

--- Eastlaws/src/Eastlaws/Controllers/AhkamController.cs
+++ Eastlaws/src/Eastlaws/Controllers/AhkamController.cs
@@ -75,6 +75,7 @@ namespace Eastlaws.Controllers
             AhkamPresentation Model =  AhkamService.Search(Options, MadaObj, TasfeyaSelection);
             if (Model.IsValid)
             {
+                ViewBag.typeView = 2;
                 return View("SearchResult", Model);
             }
             else
