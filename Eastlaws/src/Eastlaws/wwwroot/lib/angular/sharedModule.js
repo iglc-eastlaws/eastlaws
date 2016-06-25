12fc6e8e7c087d15528ba88bd83aa0346df34f7d
alnahas <alnahas1@hotmail.com>
2016-06-25 15:25:05 +0200

    image

--- Eastlaws/src/Eastlaws/wwwroot/lib/angular/sharedModule.js
+++ Eastlaws/src/Eastlaws/wwwroot/lib/angular/sharedModule.js
@@ -1087,4 +1087,18 @@
             if (predicate.call(context, this[i], i, this) === true) arr.push(this[i]);
         return arr;
     };
+
+
+    function getParameterByName(name) {
+        name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+        var regexS = "[\\?&]" + name + "=([^&#]*)";
+
+        var regex = new RegExp(regexS);
+        var results = regex.exec(window.location.href);
+        if (results == null)
+            return "";
+        else
+            return decodeURIComponent(results[1].replace(/\+/g, " "));
+    }
+
 })();
