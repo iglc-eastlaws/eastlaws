483f44815ed697ab689956b09849e3df100814ea
alnahas <alnahas1@hotmail.com>
2016-06-26 16:32:59 +0200

    tiff to jpg ver1

---
+++ Eastlaws/src/Eastlaws/Infrastructure/ImgProcess.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Eastlaws.Infrastructure
+{
+    public class ImgProcess
+    {
+        public static string[] ConvertTiffToJpeg(string fileName)
+        {
+            using (Image imageFile = Image.FromFile(fileName))
+            {
+                FrameDimension frameDimensions = new FrameDimension(imageFile.FrameDimensionsList[0]);
+                int frameNum = imageFile.GetFrameCount(frameDimensions);
+                string[] jpegPaths = new string[frameNum];
+
+                for (int frame = 0; frame < frameNum; frame++)
+                {
+                    imageFile.SelectActiveFrame(frameDimensions, frame);
+                    using (Bitmap bmp = new Bitmap(imageFile))
+                    {
+                        jpegPaths[frame] = String.Format("{0}\\{1}.jpg", Path.GetDirectoryName(fileName), Path.GetFileNameWithoutExtension(fileName));
+                        bmp.Save(jpegPaths[frame], ImageFormat.Jpeg);
+                    }
+                }
+
+                return jpegPaths;
+            }
+        }
+    }
+}
