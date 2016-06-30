06b188543d2491964dcd6f9dd4d4297774f38303
Nada AbuGamel <nobi_nh@hotmail.com>
2016-06-30 15:32:29 +0200

    convert

--- Eastlaws/src/Eastlaws/Services/ImageService.cs
+++ Eastlaws/src/Eastlaws/Services/ImageService.cs
@@ -10,6 +10,8 @@ using System.Data.SqlClient;
 using System.IO;
 using System.Drawing;
 using System.Drawing.Imaging;
+using Microsoft.AspNet.Hosting;
+using Microsoft.Extensions.PlatformAbstractions;
 
 namespace Eastlaws.Services
 {
@@ -17,6 +19,15 @@ namespace Eastlaws.Services
     {
         public static string ClearFolder = "C:\\Images\\";
         public static string ConvertedFolder = "C:\\ConvertedImages\\";
+        private readonly IApplicationEnvironment _appEnvironment;
+        public static string rootpath = "";
+
+        public ImageService(IApplicationEnvironment appEnvironment)
+        {
+            _appEnvironment = appEnvironment;
+            var rootPath = _appEnvironment.ApplicationBasePath;
+        }
+
 
         public static IEnumerable<Images> GetImages(int ServiceID)
         {
@@ -48,49 +59,60 @@ namespace Eastlaws.Services
 
         public static bool CopyRight(string oldPath, string NewPath, byte RecType, int MasterID, int ImageID)
         {
-            //Image imgLogo = Image.FromFile(HostingEnvironment.MapPath("~/CopyRight.gif"));
-            //int lWidth = imgLogo.Width, lHeight = imgLogo.Height;
+          
+            Image imgLogo = Image.FromFile(rootpath + "~/CopyRight.gif");
+            int lWidth = imgLogo.Width, lHeight = imgLogo.Height;
 
-            //Image imgOriginal = Image.FromFile(oldPath);
-            //int w = imgOriginal.Width, h = imgOriginal.Height;
-            //Size newSize = getScaledSize(new Size(w, h));
-            //w = newSize.Width;
-            //h = newSize.Height;
-
-
-            //Bitmap bitMap = new Bitmap(w, h + 80, PixelFormat.Format16bppRgb555);
-            //Graphics g = Graphics.FromImage(bitMap);
-
-            //g.DrawImage(imgOriginal, new Rectangle(0, 80, w, h));
+            Image imgOriginal = Image.FromFile(oldPath);
+            int w = imgOriginal.Width, h = imgOriginal.Height;
+            Size newSize = getScaledSize(new Size(w, h));
+            w = newSize.Width;
+            h = newSize.Height;
 
 
-            //Image imgWaterMrk = Image.FromFile(HostingEnvironment.MapPath("~/Watermark.png"));
-            //Size wtrMarkSize = imgWaterMrk.Size;
-            //if (wtrMarkSize.Width > w || wtrMarkSize.Height > h)
-            //    wtrMarkSize = getScaledSize(imgWaterMrk.Size, newSize);
+            Bitmap bitMap = new Bitmap(w, h + 80, PixelFormat.Format16bppRgb555);
+            Graphics g = Graphics.FromImage(bitMap);
 
-            //int x1 = (w - wtrMarkSize.Width) / 2;
-            //int y1 = ((h - wtrMarkSize.Height) / 2) + 80;
-
-            //g.DrawImage(imgWaterMrk, x1, y1, wtrMarkSize.Width, wtrMarkSize.Height);
+            g.DrawImage(imgOriginal, new Rectangle(0, 80, w, h));
 
 
-            //g.FillRectangle(Brushes.White, 0, 0, w, 80);
-            //g.DrawRectangle(Pens.Black, 0, 0, w - 2, 80);
-            //if (lWidth <= w)
-            //{
-            //    int x;
-            //    x = (w / 2) - (lWidth / 2);
-            //    x -= 50;
-            //    g.DrawImage(imgLogo, x, 10);
-            //}
+            Image imgWaterMrk = Image.FromFile(rootpath + "~/Watermark.png");
+            Size wtrMarkSize = imgWaterMrk.Size;
+            if (wtrMarkSize.Width > w || wtrMarkSize.Height > h)
+                wtrMarkSize = getScaledSize(imgWaterMrk.Size, newSize);
 
-            //bitMap.Save(NewPath, ImageFormat.Gif);
-            //bitMap.Dispose();
-            //bitMap = null;
+            int x1 = (w - wtrMarkSize.Width) / 2;
+            int y1 = ((h - wtrMarkSize.Height) / 2) + 80;
+
+            g.DrawImage(imgWaterMrk, x1, y1, wtrMarkSize.Width, wtrMarkSize.Height);
+
+
+            g.FillRectangle(Brushes.White, 0, 0, w, 80);
+            g.DrawRectangle(Pens.Black, 0, 0, w - 2, 80);
+            if (lWidth <= w)
+            {
+                int x;
+                x = (w / 2) - (lWidth / 2);
+                x -= 50;
+                g.DrawImage(imgLogo, x, 10);
+            }
+
+            bitMap.Save(NewPath, ImageFormat.Gif);
+            bitMap.Dispose();
+            bitMap = null;
 
             //DAL.ExecuteSQL(string.Format("Update ConvertedImages Set Width={0} , Height={1} Where RecType={2} And MasterID={3} and ID={4}", w, h, RecType, MasterID, ImageID));
             return true;
+        }
+
+        private static Size getScaledSize(Size big)
+        {
+            Size small = new Size(1200, 1400);
+            decimal ratio = Math.Max(Decimal.Divide(big.Width, small.Width), Decimal.Divide(big.Height, small.Height));
+            int nWidth = Convert.ToInt32(Decimal.Divide(big.Width, ratio));
+            int nheight = Convert.ToInt32(Decimal.Divide(big.Height, ratio));
+            return new Size(nWidth, nheight);
+
         }
 
         private static Size getScaledSize(Size big, Size small)
